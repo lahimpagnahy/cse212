df6680ca718ce9f10b879505af0932a3bb774943
lahimpagnahy <lahimpagnahy@gmail.com>
2026-07-11 20:28:02 +0300

    Implement RotateListRight and MultiplesOf methods
    Added methods to rotate a list and generate multiples of a number.

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -31,3 +31,77 @@ public static class Arrays
         // be implemented by another person.
     }
 }
+
+
+ /// <summary>
+    /// Rotate the 'data' to the right by the 'amount'.  For example, if the data is 
+    /// List<int>{1, 2, 3, 4, 5, 6, 7, 8, 9} and an amount is 3 then the list after the function runs should be 
+    /// List<int>{7, 8, 9, 1, 2, 3, 4, 5, 6}.  The value of amount will be in the range of 1 to data.Count, inclusive.
+    ///
+    /// Because a list is dynamic, this function will modify the existing data list rather than returning a new list.
+    /// </summary>
+    public static void RotateListRight(List<int> data, int amount)
+    {
+        // TODO Problem 2 Start
+        // Remember: Using comments in your program, write down your process for solving this problem
+        // step by step before you write the code. The plan should be clear enough that it could
+        // be implemented by another person.
+
+        public static class Arrays
+{
+    /// <summary>
+    /// This function will produce an array of size 'length' starting with 'number' followed by multiples of 'number'.  For 
+    /// example, MultiplesOf(7, 5) will result in: {7, 14, 21, 28, 35}.  Assume that length is a positive
+    /// integer greater than 0.
+    /// </summary>
+    /// <returns>array of doubles that are the multiples of the supplied number</returns>
+    public static double[] MultiplesOf(double number, int length)
+    {
+        // Plan:
+        // 1. Create a new array sized to hold "length" values.
+        // 2. Loop from 0 to length - 1.
+        // 3. Each multiple = number * (loop index + 1).
+        //    (index 0 -> 1st multiple, index 1 -> 2nd multiple, etc.)
+        // 4. Store the multiple in the array at the current index.
+        // 5. Return the completed array.
+
+        double[] multiples = new double[length];
+
+        for (int i = 0; i < length; i++)
+        {
+            multiples[i] = number * (i + 1);
+        }
+
+        return multiples;
+    }
+
+    /// <summary>
+    /// Rotate the 'data' to the right by the 'amount'.  For example, if the data is 
+    /// List<int>{1, 2, 3, 4, 5, 6, 7, 8, 9} and an amount is 3 then the list after the function runs should be 
+    /// List<int>{7, 8, 9, 1, 2, 3, 4, 5, 6}.  The value of amount will be in the range of 1 to data.Count, inclusive.
+    ///
+    /// Because a list is dynamic, this function will modify the existing data list rather than returning a new list.
+    /// </summary>
+    public static void RotateListRight(List<int> data, int amount)
+    {
+        // Plan:
+        // 1. Rotating right by "amount" means the last "amount" elements
+        //    should end up at the front, followed by the remaining elements
+        //    in their original order.
+        // 2. Use GetRange to slice the list into two pieces:
+        //    - front = everything except the last "amount" elements
+        //    - tail = the last "amount" elements
+        // 3. Clear the original list.
+        // 4. Add the tail piece back first, then the front piece.
+        //    This produces the rotated order without shifting elements one at a time.
+
+        int splitIndex = data.Count - amount;
+
+        List<int> front = data.GetRange(0, splitIndex);
+        List<int> tail = data.GetRange(splitIndex, amount);
+
+        data.Clear();
+        data.AddRange(tail);
+        data.AddRange(front);
+    }
+}
