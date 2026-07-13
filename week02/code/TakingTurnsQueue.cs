8973f1b53f7214d7016551b0bde6c35e3b539816
lahimpagnahy <lahimpagnahy@gmail.com>
2026-07-13 16:05:01 +0300

    Refactor TakingTurnsQueue dequeue logic

--- week02/code/TakingTurnsQueue.cs
+++ week02/code/TakingTurnsQueue.cs
@@ -37,17 +37,22 @@ public class TakingTurnsQueue
         {
             throw new InvalidOperationException("No one in the queue.");
         }
-        else
-        {
-            Person person = _people.Dequeue();
-            if (person.Turns > 1)
-            {
-                person.Turns -= 1;
-                _people.Enqueue(person);
-            }
 
-            return person;
+        Person person = _people.Dequeue();
+
+        if (person.Turns > 1)
+        {
+            person.Turns -= 1;
+            _people.Enqueue(person);
         }
+        else if (person.Turns <= 0)
+        {
+            // Infinite turns - always goes back in without decrementing
+            _people.Enqueue(person);
+        }
+        // else: Turns == 1, this was their last turn, don't re-enqueue
+
+        return person;
     }
 
     public override string ToString()
