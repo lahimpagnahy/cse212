b3865da6dec0e62fc36d904842649c7a0a29464b
lahimpagnahy <lahimpagnahy@gmail.com>
2026-07-13 16:12:13 +0300

    Fix bugs in PriorityQueue Dequeue method
    Fixed bugs in the Dequeue method to ensure correct priority handling and adjusted loop conditions.

--- week02/code/PriorityQueue.cs
+++ week02/code/PriorityQueue.cs
@@ -1,7 +1,6 @@
-﻿public class PriorityQueue
+public class PriorityQueue
 {
     private List<PriorityItem> _queue = new();
-
     /// <summary>
     /// Add a new value to the queue with an associated priority.  The
     /// node is always added to the back of the queue regardless of 
@@ -14,27 +13,24 @@
         var newNode = new PriorityItem(value, priority);
         _queue.Add(newNode);
     }
-
     public string Dequeue()
     {
         if (_queue.Count == 0) // Verify the queue is not empty
         {
             throw new InvalidOperationException("The queue is empty.");
         }
-
         // Find the index of the item with the highest priority to remove
         var highPriorityIndex = 0;
-        for (int index = 1; index < _queue.Count - 1; index++)
+        for (int index = 1; index < _queue.Count; index++)
         {
-            if (_queue[index].Priority >= _queue[highPriorityIndex].Priority)
+            if (_queue[index].Priority > _queue[highPriorityIndex].Priority)
                 highPriorityIndex = index;
         }
-
         // Remove and return the item with the highest priority
         var value = _queue[highPriorityIndex].Value;
+        _queue.RemoveAt(highPriorityIndex);
         return value;
     }
-
     // DO NOT MODIFY THE CODE IN THIS METHOD
     // The graders rely on this method to check if you fixed all the bugs, so changes to it will cause you to lose points.
     public override string ToString()
@@ -42,18 +38,15 @@
         return $"[{string.Join(", ", _queue)}]";
     }
 }
-
 internal class PriorityItem
 {
     internal string Value { get; set; }
     internal int Priority { get; set; }
-
     internal PriorityItem(string value, int priority)
     {
         Value = value;
         Priority = priority;
     }
-
     // DO NOT MODIFY THE CODE IN THIS METHOD
     // The graders rely on this method to check if you fixed all the bugs, so changes to it will cause you to lose points.
     public override string ToString()
